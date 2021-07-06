41c7f52bc1e88bccdf99726bbefa433986b061f7
josedavms13 <josedav0ms13@gmail.com>
2021-07-06 07:48:43 -0500

    FEAT: clock fixed. Styling

--- classes/languageManager.js
+++ classes/languageManager.js
@@ -29,7 +29,7 @@ class languageManager {
             
          case 'spanish':
             return `
-               <h4>Porfavor  considera ver mi portafolio en react</h4>
+               <h4>Porfavor  considera ver mi portafolio en React</h4>
                <button onclick="shouldIStayOrShouldIGo('react')" id="React-button" class="react-button"> Muestrame el portafolio de React</button>
                <button onclick="shouldIStayOrShouldIGo()" id="Vanilla-button" class="vanilla-button">Dejame aquí en la version vanilla</button>
             `
@@ -67,5 +67,41 @@ class languageManager {
             
       }  
    }
+
+   setWelcome(experience){
+      switch (this.language){
+         case 'english':
+            document.querySelector('.welcome').innerHTML = `
+                   <h1 id="Welcome-title">Welcome to my portfolio</h1>
+                   <h3>My name is Jose David Esmoris and I am a frontend developer</h3>
+                   <h4>I have ${experience} years of experience</h4>
+                   <h4>and I'd like to be part of your team</h4>
+            `
+            break
+         case 'spanish':
+            document.querySelector('.welcome').innerHTML = `
+                   <h1 id="Welcome-title">Bienvenid@ a mi portafolio</h1>
+                   <h3>Yo soy Jose David Esmoris y soy un desarrollador frontend</h3>
+                   <h4>Tengo ${experience} años de experiencia</h4>
+                   <h4>y quisiera ser parte de tu equipo</h4>
+
+            `
+            break
+      }
+   }
+
+   setExperienceTittles(){
+      switch (this.language){
+         case 'english':
+            document.getElementById('Real-projects-title').innerText = `Real Projects`
+            document.getElementById('Practice-projects-title').innerText = `Practice Projects`
+            break
+         case 'spanish':
+            document.getElementById('Real-projects-title').innerText = `Proyectos reales`
+            document.getElementById('Practice-projects-title').innerText = `Proyectos de Práctica`
+            break
+      }
+   }
+
 
 }
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import setHtmlText from "./utilities/setHtmlText.js"
 
 //region GLOBAL VARIABLES
 let LANGUAGE_STATE;
-let USER_NAME;
 
 
 //endregion global variables
@@ -16,13 +15,12 @@ let USER_NAME;
 const spanishButton = document.getElementById('Spanish-button');
 const englishButton = document.getElementById('English-button');
 
-spanishButton.addEventListener('click', ()=>languageManagement('spanish'))
-englishButton.addEventListener('click', ()=>languageManagement('english'))
+spanishButton.addEventListener('click', () => languageManagement('spanish'))
+englishButton.addEventListener('click', () => languageManagement('english'))
 //endregion language picker
 
 //region Input Fields
 const userNameInputField = document.getElementById('Name-field');
-
 
 
 //endregion Input Fields
@@ -31,49 +29,65 @@ const userNameInputField = document.getElementById('Name-field');
 //region react invitation
 
 const reactButton = document.getElementById('React-button');
-reactButton.addEventListener('React-button', ()=>shouldIStayOrShouldIGo('react'));
+reactButton.addEventListener('React-button', () => shouldIStayOrShouldIGo('react'));
 
 const vanillaButton = document.getElementById('Vanilla-button');
-reactButton.addEventListener('Vanilla-button', ()=>shouldIStayOrShouldIGo('vanilla'));
+reactButton.addEventListener('Vanilla-button', () => shouldIStayOrShouldIGo('vanilla'));
 
 //endregion react invitation
 
 
 //endregion event Listeners
 
+const languageSelection = document.getElementById("Language-selection");
+const reactInvitation = document.getElementById("React-invitation-container");
+
+const sectionClassSection = document.querySelector('.section')
 //region language Management
 
-function languageManagement(language){
-    LANGUAGE_STATE =  new languageManager(language);
+function languageManagement(language) {
+
+    LANGUAGE_STATE = new languageManager(language);
+
+    //region show and hide
+    document.querySelector('header').classList.remove('d-none');
+
+    // Display react options
+    languageSelection.classList.add('d-none');
+    reactInvitation.classList.remove('d-none');
+    //endregion show and hide
+
+
     //Invitation to React Portfolio
     setHtmlText('React-invitation-container', LANGUAGE_STATE.reactInvitation);
 
     //Navigation container
     setHtmlText('Nav-list-container', LANGUAGE_STATE.navbar);
-    document.querySelector('header').classList.remove('d-none');
 
-    setUserName(USER_NAME);
+    //welcome
+    LANGUAGE_STATE.setWelcome()
 
-    // Display react options
-    document.getElementById("React-invitation-container").classList.remove('d-none');
-    document.getElementById("Language-selection").classList.add('d-none');
+
+    //Experience section
+    LANGUAGE_STATE.setExperienceTittles();
 
 }
+
+
+
+
+
+
+
+
 
 
 //endregion language Management
 
 
-
-
-
-
-
-
-
 //show welcome
 
-function welcome(){
+function welcome() {
     const welcomePanel = document.getElementById('Welcome');
 
 
@@ -81,24 +95,17 @@ function welcome(){
 
 welcome();
 
-// user input name
-function setUserName(){
-
-    USER_NAME = userNameInputField.value;
-
-    console.log(USER_NAME);
-
-}
-
 // react invitation
 
-function shouldIStayOrShouldIGo(technology){
+function shouldIStayOrShouldIGo(technology) {
 
     console.log('clicked')
 
+    sectionClassSection.classList.remove('d-none');
+
     const panel = document.getElementById('React-invitation-container');
 
-    switch(technology){
+    switch (technology) {
         case 'react':
             console.log('to react portfolio');
             break
@@ -112,26 +119,20 @@ function shouldIStayOrShouldIGo(technology){
             break
     }
 
-
-
-
 }
 
 
+//region Clock functions
 
-
-
-//Clock functions
-
-setInterval(setClock, 1000)
+const clockFunction = setInterval(setClock, 1000)
 
 const secondHand = document.querySelector('[data-second-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const hourHand = document.querySelector('[data-hour-hand]')
 
-function setClock(){
+function setClock() {
     const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds() /60;
+    const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
 
@@ -141,11 +142,60 @@ function setClock(){
 }
 
 
-function setRotation(element, rotation){
+function setRotation(element, rotation) {
     element.style.setProperty('--rotation', rotation * 360);
 }
 
 setClock();
+
+//endregion Clock functions
+
+
+// Animations
+
+const clock = document.getElementById('Clock-container');
+
+let hoverInTL = gsap.timeline();
+let CLOCK_OPEN = false;
+
+function clockAmp() {
+    CLOCK_OPEN = !CLOCK_OPEN;
+    if (CLOCK_OPEN) {
+        hoverInTL.play();
+    } else {
+        hoverInTL.reverse();
+    }
+}
+
+hoverInTL.pause();
+
+hoverInTL.to('#Clock', {
+    position: 'absolute',
+    duration: 1,
+    top: '50%',
+    left: '15vw',
+    height: '80vw',
+    width: '80vw',
+})
+    .to('.number', {
+        duration: 0.5,
+        opacity: 1,
+        delay: 0.1,
+        ease: 'power4.inOut',
+    })
+    .from('.hand', {
+        width: 4,
+        background: 'white',
+        opacity: 1,
+        duration: 0.5,
+
+    }, '.analog-watch .hand.second',{
+        opacity : 1,
+    })
+
+clock.addEventListener('click', clockAmp)
+
+
 
 //DOM functions
 window.shouldIStayOrShouldIGo = shouldIStayOrShouldIGo
